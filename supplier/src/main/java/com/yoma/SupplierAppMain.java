088f7adba464c8b52e529241e94ff5b2fd72d80c
yordan-marinov <jordanmarinov8@gmail.com>
2022-11-27 16:03:31 +0200

    Added docker compose for kafka

--- supplier/src/main/java/com/yoma/SupplierAppMain.java
+++ supplier/src/main/java/com/yoma/SupplierAppMain.java
@@ -2,11 +2,22 @@ package com.yoma;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.function.Supplier;
 
 @SpringBootApplication
 public class SupplierAppMain {
+
+    public static final String HELLO_SUPPLIER = "Hello Supplier!!!";
+
     public static void main(String[] args) {
         SpringApplication.run(SupplierAppMain.class, args);
-        System.out.println("Hello Supplier!!!");
+        System.out.println(HELLO_SUPPLIER);
+    }
+
+    @Bean
+    public Supplier<String> supplier() {
+        return () -> HELLO_SUPPLIER;
     }
 }
