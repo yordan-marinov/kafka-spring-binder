2467d215e26db015dd7a79fafec276c6b28e7a3f
yordan-marinov <jordanmarinov8@gmail.com>
2022-11-27 16:19:10 +0200

    Added functions

--- consumer/src/main/java/com/yoma/ConsumerAppMain.java
+++ consumer/src/main/java/com/yoma/ConsumerAppMain.java
@@ -2,11 +2,24 @@ package com.yoma;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.function.Consumer;
 
 @SpringBootApplication
 public class ConsumerAppMain {
+
+    public static final String HELLO_CONSUMER = "Hello Consumer!!!";
+
     public static void main(String[] args) {
         SpringApplication.run(ConsumerAppMain.class, args);
-        System.out.println("Hello Consumer!!!");
+        System.out.println(HELLO_CONSUMER);
+
+
+    }
+
+    @Bean
+    public Consumer<String> consumer() {
+        return System.out::println;
     }
 }
